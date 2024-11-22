698f562dcbb7a23562698bef0a6d29bad5c0e88a
ertreiter <guardian98x@gmail.com>
2024-11-23 00:10:40 +0800

    profile

---
+++ app/src/main/java/com/manikandareas/codileap/chatbot/presentation/component/ChatBotAppBar.kt
@@ -0,0 +1,49 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.manikandareas.codileap.chatbot.presentation.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.manikandareas.codileap.auth.presentation.AuthAction
+import com.manikandareas.codileap.ui.theme.CodiLeapTheme
+
+@Composable
+fun ChatBotAppBar(modifier: Modifier = Modifier,onBackClicked: (AuthAction) -> Unit = {}) {
+    TopAppBar(
+        title = {
+            Text(text = "Chat Bot", style = MaterialTheme.typography.titleLarge)
+        },
+        navigationIcon = {
+            IconButton(
+                onClick = { onBackClicked(AuthAction.OnBackClicked) },
+                modifier = Modifier.padding(start = 12.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBackIosNew,
+                    contentDescription = "Back",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
+    )
+}
+
+@PreviewLightDark
+@Composable
+fun PreviewChatBotAppBar(modifier: Modifier = Modifier) {
+    CodiLeapTheme {
+        ChatBotAppBar(modifier = modifier)
+    }
+}
