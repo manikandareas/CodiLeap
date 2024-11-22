1063943b90e3d64fa7d238c9bfb687a392dbb97f
ertreiter <guardian98x@gmail.com>
2024-11-23 00:10:40 +0800

    profile

--- app/src/main/java/com/manikandareas/codileap/chatbot/presentation/ChatBotScreen.kt
+++ app/src/main/java/com/manikandareas/codileap/chatbot/presentation/ChatBotScreen.kt
@@ -2,11 +2,16 @@
 
 package com.manikandareas.codileap.chatbot.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.shape.RoundedCornerShape
 import com.manikandareas.codileap.auth.presentation.AuthAction
 import com.manikandareas.codileap.chatbot.presentation.component.ChatBotAppBar
 import com.manikandareas.codileap.chatbot.presentation.component.ChatBotChat
